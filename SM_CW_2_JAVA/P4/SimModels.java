1b3152ab6f698337c9c429b2ac47632f411affe5
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-22 02:39:15 +0300

    Do refactor.

--- SM_CW_2_JAVA/P4/SimModels.java
+++ SM_CW_2_JAVA/P4/SimModels.java
@@ -1,21 +1,22 @@
 package SM_CW_2_JAVA.P4;
 
-
 import SM_CW_2_JAVA.P2.simsimple.Model;
 
 import static SM_CW_2_JAVA.P3.SimModel.createModel;
 
 public class SimModels {
     public static void main(String[] args) {
-        double[] createTimes = {.5, 1.0, 2.0, 3.0, 4.0};
-        double[] processTimes = {.5, 1.0, 2.0, 3.0, 4.0};
+        double[] createTimes = {.5, 1., 2., 3., 4.};
+        double[] processTimes = {.5, 1., 2., 3., 4.};
         int[] maxQueues = {5, -1}; // -1 means no limit
 
         for (int maxQueue : maxQueues) {
             for (double createDelay : createTimes) {
                 for (double processDelay : processTimes) {
-                    Model model = createModel(createDelay, processDelay, maxQueue);
-                    model.simulate(3000.0);
+                    System.out.printf("\n\n\nCreate delay: %.1f, Process delay: %.1f, Max queue: %d\n", createDelay,
+                            processDelay, maxQueue);
+                    Model model = createModel(createDelay, processDelay, maxQueue, false);
+                    model.simulate(1000.);
                     model.printResult();
                 }
             }
