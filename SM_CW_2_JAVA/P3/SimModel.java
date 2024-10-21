68ffcb3a7f9058d8fcd6228d6a271d2fb86903c2
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-22 02:39:22 +0300

    Do refactor.

--- SM_CW_2_JAVA/P3/SimModel.java
+++ SM_CW_2_JAVA/P3/SimModel.java
@@ -1,8 +1,6 @@
 package SM_CW_2_JAVA.P3;
 
-
 import SM_CW_2_JAVA.P1.simsimple.Create;
-import SM_CW_2_JAVA.P1.simsimple.Element;
 import SM_CW_2_JAVA.P1.simsimple.IElement;
 import SM_CW_2_JAVA.P2.simsimple.Model;
 import SM_CW_2_JAVA.P2.simsimple.Process;
@@ -14,26 +12,22 @@ import java.util.Collections;
 
 public class SimModel {
     public static void main(String[] args) {
-        Model model = createModel(1.0, 1.0, 5);
-        model.simulate(3000.0);
+        Model model = createModel(1., 1., 5, true);
+        model.simulate(1000.);
         model.printResult();
     }
 
-    @Contract("_, _, _ -> new")
-    public static @NotNull Model createModel(double cDelay, double pDelay, int pMaxQueue) {
-        Create c = new Create(cDelay);
-        c.setName("CREATOR");
+    @Contract("_, _, _, _ -> new")
+    public static @NotNull Model createModel(double cDelay, double pDelay, int pMaxQueue, boolean verbose) {
+        Create c = new Create("CREATOR", cDelay);
         c.setDistribution("exp");
         Process[] processes = new Process[3];
         for (int i = 0; i < processes.length; ++i) {
-            processes[i] = new Process(pDelay);
-            processes[i].setMaxQueue(pMaxQueue);
-            processes[i].setDistribution("exp");
-            processes[i].setName("PROCESSOR_" + (i + 1));
+            processes[i] = new Process("PROCESSOR_" + (i + 1), pDelay, pMaxQueue);
         }
-        System.out.print("id0 = " + c.getId());
+        System.out.printf("id0=%d", c.getId());
         for (int i = 0; i < processes.length; ++i) {
-            System.out.print(" id" + (i + 1) + "=" + processes[i].getId());
+            System.out.printf(" id%d=%d", i + 1, processes[i].getId());
         }
         c.setNextElement(processes[0]);
         for (int i = 0; i < processes.length - 1; ++i) {
@@ -42,6 +36,6 @@ public class SimModel {
         ArrayList<IElement> list = new ArrayList<>();
         list.add(c);
         Collections.addAll(list, processes);
-        return new Model(list);
+        return new Model(list, verbose);
     }
 }
