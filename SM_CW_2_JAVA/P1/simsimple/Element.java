46e98bb8c305f82754238e0f5bc1e1371a8a9fda
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-22 02:39:06 +0300

    Do refactor.

--- SM_CW_2_JAVA/P1/simsimple/Element.java
+++ SM_CW_2_JAVA/P1/simsimple/Element.java
@@ -1,50 +1,57 @@
 package SM_CW_2_JAVA.P1.simsimple;
 
-public class Element implements  IElement {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class Element implements IElement, Comparable<IElement> {
+    private static int nextId = 0;
     private String name;
-    private double tnext;
+    private double tNext;
     private double delayMean, delayDev;
     private String distribution;
     private int quantity;
-    private double tcurr;
+    private double tCurr;
     private int state;
     private IElement nextElement;
-    private static int nextId=0;
     private int id;
 
-    public Element(){
-        tnext = Double.MAX_VALUE;
-        delayMean = 1.0;
+    public Element() {
+        tNext = Double.MAX_VALUE;
+        delayMean = 1.;
         distribution = "exp";
-        tcurr = tnext;
-        state=0;
-        nextElement=null;
+        tCurr = tNext;
+        state = 0;
+        nextElement = null;
         id = nextId;
         ++nextId;
-        name = "element"+id;
+        name = "element" + id;
     }
-    public Element(double delay){
-        tnext = 0.0;
+
+    public Element(double delay) {
+        tNext = .0;
         delayMean = delay;
         distribution = "";
-        tcurr = tnext;
-        state=0;
-        nextElement=null;
+        tCurr = tNext;
+        state = 0;
+        nextElement = null;
         id = nextId;
         ++nextId;
-        name = "element"+id;
+        name = "element" + id;
     }
-    public Element(String nameOfElement, double delay){
-        tnext = 0.0;
+
+    public Element(String nameOfElement, double delay) {
+        tNext = .0;
         delayMean = delay;
         distribution = "exp";
-        tcurr = tnext;
-        state=0;
-        nextElement=null;
+        tCurr = tNext;
+        state = 0;
+        nextElement = null;
         id = nextId;
         ++nextId;
         name = nameOfElement;
     }
+
     @Override
     public double getDelay() {
         double delay = getDelayMean();
@@ -52,32 +59,35 @@ public class Element implements  IElement {
             delay = FunRand.Exp(getDelayMean());
         } else {
             if ("norm".equalsIgnoreCase(getDistribution())) {
-                delay = FunRand.Norm(getDelayMean(),
-                        getDelayDev());
+                delay = FunRand.Norm(getDelayMean(), getDelayDev());
             } else {
                 if ("unif".equalsIgnoreCase(getDistribution())) {
-                    delay = FunRand.Unif(getDelayMean(),
-                            getDelayDev());
+                    delay = FunRand.Unif(getDelayMean(), getDelayDev());
                 } else {
-                    if("".equalsIgnoreCase(getDistribution())){
-                        delay = getDelayMean();}
+                    if ("".equalsIgnoreCase(getDistribution())) {
+                        delay = getDelayMean();
+                    }
                 }
             }
         }
         return delay;
     }
+
     @Override
     public double getDelayDev() {
         return delayDev;
     }
+
     @Override
     public void setDelayDev(double delayDev) {
         this.delayDev = delayDev;
     }
+
     @Override
     public String getDistribution() {
         return distribution;
     }
+
     @Override
     public void setDistribution(String distribution) {
         this.distribution = distribution;
@@ -99,88 +109,106 @@ public class Element implements  IElement {
     }
 
     @Override
-    public double getTcurr() {
-        return tcurr;
+    public double getTCurr() {
+        return tCurr;
     }
+
     @Override
-    public void setTcurr(double tcurr) {
-        this.tcurr = tcurr;
+    public void setTCurr(double tCurr) {
+        this.tCurr = tCurr;
     }
+
     @Override
     public int getState() {
         return state;
     }
-    @Override
-    public void addState(int stateToAdd) {
-        state += stateToAdd;
-    }
+
     @Override
     public void setState(int state) {
         this.state = state;
     }
+
+    @Override
+    public void addState(int stateToAdd) {
+        state += stateToAdd;
+    }
+
     @Override
     public IElement getNextElement() {
         return nextElement;
     }
+
     @Override
     public void setNextElement(IElement nextElement) {
         this.nextElement = nextElement;
     }
+
     @Override
     public void inAct() {
-
     }
+
     @Override
-    public void outAct(){
+    public void outAct() {
         incQuantity();
     }
 
     @Override
-    public double getTnext() {
-        return tnext;
+    public double getTNext() {
+        return tNext;
     }
+
     @Override
-    public void setTnext(double tnext) {
-        this.tnext = tnext;
+    public void setTNext(double tNext) {
+        this.tNext = tNext;
     }
+
     @Override
     public double getDelayMean() {
         return delayMean;
     }
+
     @Override
     public void setDelayMean(double delayMean) {
         this.delayMean = delayMean;
     }
+
     @Override
     public int getId() {
         return id;
     }
+
     @Override
     public void setId(int id) {
         this.id = id;
     }
 
     @Override
-    public void printResult(){
-        System.out.println(getName()+ " quantity = "+ quantity);
+    public void printResult() {
+        System.out.printf("\n%s quantity=%d\n", getName(), quantity);
     }
 
     @Override
-    public void printInfo(){
-        System.out.println(getName()+ " state= " +state+
-                " quantity = "+ quantity+
-                " tnext= "+tnext);
+    public void printInfo() {
+        System.out.printf("%s\n\ttNext=%s\n\tstate=%d\n\tquantity=%d\n", getName(),
+                getTNext() == Double.MAX_VALUE ? "+inf" : String.format("%.6f", getTNext()), state, quantity);
     }
+
     @Override
     public String getName() {
         return name;
     }
+
     @Override
     public void setName(String name) {
         this.name = name;
     }
-    @Override
-    public void doStatistics(double delta){
 
+    @Override
+    public void doStatistics(double delta) {
+    }
+
+    @Override
+    public int compareTo(@NotNull IElement o) {
+        return (new Random().nextInt(3) - 1) * o.getState();
     }
 }
--- SM_CW_2_JAVA/P1/simsimple/IElement.java
+++ SM_CW_2_JAVA/P1/simsimple/IElement.java
@@ -2,31 +2,58 @@ package SM_CW_2_JAVA.P1.simsimple;
 
 public interface IElement {
     double getDelay();
+
     double getDelayDev();
+
     void setDelayDev(double delayDev);
+
     String getDistribution();
+
     void setDistribution(String distribution);
+
     int getQuantity();
+
     void incQuantity();
+
     void addQuantity(int quantityToAdd);
-    double getTcurr();
-    void setTcurr(double tcurr);
+
+    double getTCurr();
+
+    void setTCurr(double tCurr);
+
     int getState();
-    public void addState(int stateToAdd);
+
     void setState(int state);
+
+    void addState(int stateToAdd);
+
     IElement getNextElement();
+
     void setNextElement(IElement nextElement);
+
     void inAct();
+
     void outAct();
-    double getTnext();
-    void setTnext(double tnext);
+
+    double getTNext();
+
+    void setTNext(double tNext);
+
     double getDelayMean();
+
     void setDelayMean(double delayMean);
+
     int getId();
+
     void setId(int id);
+
     void printResult();
+
     void printInfo();
+
     String getName();
+
     void setName(String name);
+
     void doStatistics(double delta);
 }
