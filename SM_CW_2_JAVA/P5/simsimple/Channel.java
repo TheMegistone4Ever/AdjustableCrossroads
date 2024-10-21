d8622c4b5cacdf36d6a99933e1bdb7fbe2fde881
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-22 02:38:43 +0300

    Do refactor.

--- SM_CW_2_JAVA/P5/simsimple/Channel.java
+++ SM_CW_2_JAVA/P5/simsimple/Channel.java
@@ -2,7 +2,7 @@ package SM_CW_2_JAVA.P5.simsimple;
 
 public class Channel {
     private int state = 0;
-    private double tnext = Double.MAX_VALUE;
+    private double tNext = Double.MAX_VALUE;
 
     public int getState() {
         return state;
@@ -12,11 +12,11 @@ public class Channel {
         this.state = state;
     }
 
-    public double getTnext() {
-        return tnext;
+    public double getTNext() {
+        return tNext;
     }
 
-    public void setTnext(double tnext) {
-        this.tnext = tnext;
+    public void setTNext(double tNext) {
+        this.tNext = tNext;
     }
 }
