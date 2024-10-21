1b816b39ce1dc5136fcaac3402301210edf33099
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-22 02:39:11 +0300

    Do refactor.

--- SM_CW_2_JAVA/P1/SimModel.java
+++ SM_CW_2_JAVA/P1/SimModel.java
@@ -1,26 +1,23 @@
 package SM_CW_2_JAVA.P1;
 
-import SM_CW_2_JAVA.P1.simsimple.*;
+import SM_CW_2_JAVA.P1.simsimple.Create;
+import SM_CW_2_JAVA.P1.simsimple.IElement;
+import SM_CW_2_JAVA.P1.simsimple.Model;
 import SM_CW_2_JAVA.P1.simsimple.Process;
 
 import java.util.ArrayList;
 
 public class SimModel {
     public static void main(String[] args) {
-        Create c = new Create(2.0);
-        Process p = new Process(1.0);
-        System.out.println("id0 = " + c.getId() + " id1=" + p.getId());
+        Create c = new Create("CREATOR", 1.);
+        Process p = new Process("PROCESSOR", 1., 5);
+        System.out.printf("id0=%d id1=%d\n", c.getId(), p.getId());
         c.setNextElement(p);
-        p.setMaxQueue(5);
-        c.setName("CREATOR");
-        p.setName("PROCESSOR");
-        c.setDistribution("exp");
-        p.setDistribution("exp");
         ArrayList<IElement> list = new ArrayList<>();
         list.add(c);
         list.add(p);
-        Model model = new Model(list);
-        model.simulate(1000.0);
+        Model model = new Model(list, true);
+        model.simulate(1000.);
         model.printResult();
     }
 }
--- SM_CW_2_JAVA/P1/simsimple/Process.java
+++ SM_CW_2_JAVA/P1/simsimple/Process.java
@@ -3,17 +3,54 @@ package SM_CW_2_JAVA.P1.simsimple;
 public class Process extends Element {
     private int queue, maxQueue, failure;
     private double meanQueue;
+
+    public Process() {
+        super();
+        queue = 0;
+        maxQueue = Integer.MAX_VALUE;
+        meanQueue = .0;
+    }
+
+    public Process(int maxQueue) {
+        super();
+        queue = 0;
+        this.maxQueue = maxQueue;
+        meanQueue = .0;
+    }
+
     public Process(double delay) {
         super(delay);
         queue = 0;
         maxQueue = Integer.MAX_VALUE;
-        meanQueue = 0.0;
+        meanQueue = .0;
     }
+
+    public Process(double delay, int maxQueue) {
+        super(delay);
+        queue = 0;
+        this.maxQueue = maxQueue;
+        meanQueue = .0;
+    }
+
+    public Process(String nameOfElement, double delay) {
+        super(nameOfElement, delay);
+        queue = 0;
+        maxQueue = Integer.MAX_VALUE;
+        meanQueue = .0;
+    }
+
+    public Process(String nameOfElement, double delay, int maxQueue) {
+        super(nameOfElement, delay);
+        queue = 0;
+        this.maxQueue = maxQueue;
+        meanQueue = .0;
+    }
+
     @Override
     public void inAct() {
         if (super.getState() == 0) {
             super.setState(1);
-            super.setTnext(super.getTcurr() + super.getDelay());
+            super.setTNext(super.getTCurr() + super.getDelay());
         } else {
             if (getQueue() < getMaxQueue()) {
                 setQueue(getQueue() + 1);
@@ -22,10 +59,11 @@ public class Process extends Element {
             }
         }
     }
+
     @Override
     public void outAct() {
         super.outAct();
-        super.setTnext(Double.MAX_VALUE);
+        super.setTNext(Double.MAX_VALUE);
         super.setState(0);
 
         if (super.getNextElement() != null) {
@@ -35,44 +73,55 @@ public class Process extends Element {
         if (getQueue() > 0) {
             setQueue(getQueue() - 1);
             super.setState(1);
-            super.setTnext(super.getTcurr() + super.getDelay());
+            super.setTNext(super.getTCurr() + super.getDelay());
         }
     }
 
     public int getFailure() {
         return failure;
     }
+
     public void incFailure() {
         ++failure;
     }
+
     public void addFailure(int failureToAdd) {
         failure += failureToAdd;
     }
+
     public int getQueue() {
         return queue;
     }
+
     public void setQueue(int queue) {
         this.queue = queue;
     }
+
     public int getMaxQueue() {
         return maxQueue;
     }
+
     public void setMaxQueue(int maxQueue) {
-        if (maxQueue >= 0){
-        this.maxQueue = maxQueue;}
+        if (maxQueue >= 0) {
+            this.maxQueue = maxQueue;
+        }
     }
+
     @Override
     public void printInfo() {
         super.printInfo();
-        System.out.println("failure = " + this.getFailure());
+        System.out.printf("\tfailure=%d", this.getFailure());
     }
+
     @Override
     public void doStatistics(double delta) {
         meanQueue += queue * delta;
     }
+
     public double getMeanQueue() {
         return meanQueue;
     }
+
     public void addMeanQueue(double meanQueueToAdd) {
         meanQueue += meanQueueToAdd;
     }
