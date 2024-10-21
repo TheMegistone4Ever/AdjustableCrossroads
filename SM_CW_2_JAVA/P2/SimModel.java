30997a4efbdf70a626f1e29ba688c7f2de2e440d
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-22 02:39:19 +0300

    Do refactor.

--- SM_CW_2_JAVA/P2/SimModel.java
+++ SM_CW_2_JAVA/P2/SimModel.java
@@ -1,8 +1,6 @@
 package SM_CW_2_JAVA.P2;
 
-
 import SM_CW_2_JAVA.P1.simsimple.Create;
-import SM_CW_2_JAVA.P1.simsimple.Element;
 import SM_CW_2_JAVA.P1.simsimple.IElement;
 import SM_CW_2_JAVA.P2.simsimple.Model;
 import SM_CW_2_JAVA.P2.simsimple.Process;
@@ -11,20 +9,15 @@ import java.util.ArrayList;
 
 public class SimModel {
     public static void main(String[] args) {
-        Create c = new Create(2.0);
-        Process p = new Process(1.0);
-        System.out.println("id0 = " + c.getId() + " id1=" + p.getId());
+        Create c = new Create("CREATOR", 1.);
+        Process p = new Process("PROCESSOR", 1., 5);
+        System.out.printf("id0=%d id1=%d\n", c.getId(), p.getId());
         c.setNextElement(p);
-        p.setMaxQueue(5);
-        c.setName("CREATOR");
-        p.setName("PROCESSOR");
-        c.setDistribution("exp");
-        p.setDistribution("exp");
         ArrayList<IElement> list = new ArrayList<>();
         list.add(c);
         list.add(p);
-        Model model = new Model(list);
-        model.simulate(1000.0);
+        Model model = new Model(list, true);
+        model.simulate(1000.);
         model.printResult();
     }
 }
--- SM_CW_2_JAVA/P2/simsimple/Process.java
+++ SM_CW_2_JAVA/P2/simsimple/Process.java
@@ -3,10 +3,40 @@ package SM_CW_2_JAVA.P2.simsimple;
 public class Process extends SM_CW_2_JAVA.P1.simsimple.Process {
     private double meanLoad;
 
+    public Process() {
+        super();
+
+        meanLoad = .0;
+    }
+
+    public Process(int maxQueue) {
+        super(maxQueue);
+
+        meanLoad = .0;
+    }
+
     public Process(double delay) {
         super(delay);
 
-        meanLoad = 0.0;
+        meanLoad = .0;
+    }
+
+    public Process(double delay, int maxQueue) {
+        super(delay, maxQueue);
+
+        meanLoad = .0;
+    }
+
+    public Process(String nameOfElement, double delay) {
+        super(nameOfElement, delay);
+
+        meanLoad = .0;
+    }
+
+    public Process(String nameOfElement, double delay, int maxQueue) {
+        super(nameOfElement, delay, maxQueue);
+
+        meanLoad = .0;
     }
 
     @Override
@@ -15,14 +45,17 @@ public class Process extends SM_CW_2_JAVA.P1.simsimple.Process {
 
         meanLoad += super.getState() * delta;
     }
+
     @Override
     public void printInfo() {
         super.printInfo();
-        System.out.println("meanLoad = " + this.getMeanLoad());
+        System.out.printf("\n\tmeanLoad=%.6f\n", this.getMeanLoad());
     }
+
     public double getMeanLoad() {
         return meanLoad;
     }
+
     public void addMeanLoad(double meanLoadToAdd) {
         meanLoad += meanLoadToAdd;
     }
