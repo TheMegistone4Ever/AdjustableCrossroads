1b507f7c92dfa6b31546ce5c9575c4d8fb323929
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-27 02:00:33 +0300

    Done PoC of improved MON algorithm.

---
+++ SM_CW_3_JAVA/P2/SimModel.java
@@ -0,0 +1,7 @@
+package SM_CW_3_JAVA.P2;
+
+public class SimModel {
+    public static void main(String[] args) {
+        System.out.println("2) Hello, World!");
+    }
+}
---
+++ SM_CW_3_JAVA/P3/SimModel.java
@@ -0,0 +1,7 @@
+package SM_CW_3_JAVA.P3;
+
+public class SimModel {
+    public static void main(String[] args) {
+        System.out.println("3) Hello, World!");
+    }
+}
