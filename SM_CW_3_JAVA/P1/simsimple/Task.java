83556d8195ac56529807e92bbed5c226e1a3afff
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-27 02:00:26 +0300

    Done PoC of improved MON algorithm.

---
+++ SM_CW_3_JAVA/P1/simsimple/Task.java
@@ -0,0 +1,13 @@
+package SM_CW_3_JAVA.P1.simsimple;
+
+public class Task {
+    private double tNext = Double.MAX_VALUE;
+
+    public double getTNext() {
+        return tNext;
+    }
+
+    public void setTNext(double tNext) {
+        this.tNext = tNext;
+    }
+}
