0eff2a9088aafde9ae10564e86f30eb63651bd87
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-27 02:00:24 +0300

    Done PoC of improved MON algorithm.

---
+++ SM_CW_3_JAVA/P1/simsimple/Forking.java
@@ -0,0 +1,7 @@
+package SM_CW_3_JAVA.P1.simsimple;
+
+public enum Forking {
+    PROBABILISTIC,
+    PRIORITIZED,
+    COMBINED,
+}
---
+++ SM_CW_3_JAVA/P1/simsimple/Path.java
@@ -0,0 +1,76 @@
+package SM_CW_3_JAVA.P1.simsimple;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Predicate;
+
+public class Path implements Comparable<Path> {
+    private final Element to;
+    private int priority = 1;
+    private double probability = 1.;
+    private Predicate<Task> blocker = null;
+
+    public Path() {
+        this.to = null;
+    }
+
+    public Path(Element to, double probability) {
+        this.to = to;
+        this.probability = probability;
+    }
+
+    public Path(Element to, int priority) {
+        this.to = to;
+        this.priority = priority;
+    }
+
+    public Path(Element to, double probability, int priority) {
+        this.to = to;
+        this.probability = probability;
+        this.priority = priority;
+    }
+
+    public Path(Element to, double probability, int priority, Predicate<Task> blocker) {
+        this.to = to;
+        this.probability = probability;
+        this.priority = priority;
+        this.blocker = blocker;
+    }
+
+    public boolean isBlocked(Task task) {
+        return blocker != null && blocker.test(task);
+    }
+
+    public Element getTo() {
+        return to;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+
+    public Predicate<Task> getBlocker() {
+        return blocker;
+    }
+
+    public void setBlocker(Predicate<Task> blocker) {
+        this.blocker = blocker;
+    }
+
+    @Override
+    public int compareTo(@NotNull Path o) {
+        return Integer.compare(o.priority, priority);
+    }
+}
