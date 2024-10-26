59df85f85151b2a56542fcbcc597ceb044a64988
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-27 02:00:43 +0300

    Done PoC of improved MON algorithm.

---
+++ SM_CW_3_JAVA/P1/simsimple/Model.java
@@ -0,0 +1,39 @@
+package SM_CW_3_JAVA.P1.simsimple;
+
+import SM_CW_2_JAVA.P1.simsimple.IElement;
+
+import java.util.ArrayList;
+
+public class Model extends SM_CW_2_JAVA.P1.simsimple.Model {
+
+    public Model(ArrayList<IElement> elements, boolean verbose) {
+        super(elements, verbose);
+    }
+
+    @Override
+    public void simulate(double time) {
+        super.simulate(time);
+
+        // if any remains in queue, add to failure statistics
+        for (IElement e : super.getList()) {
+            if (e instanceof Process p) {
+                p.addFailure(p.getQueue().size() + p.getState());
+            }
+        }
+    }
+
+    @Override
+    public void printResult() {
+        System.out.println("\n-------------RESULTS-------------");
+        for (IElement e : super.getList()) {
+            e.printResult();
+            if (e instanceof Process p) {
+                System.out.printf(
+                        "failure = %d\nmean length of queue = %.6f\nmean load = %.6f\nfailure probability = %.6f\n",
+                        p.getFailures(), p.getAccumulatedQueue() / super.getCurrentTime(),
+                        p.getAccumulatedLoad() / super.getCurrentTime(), p.getFailures() / (double) p.getQuantity()
+                );
+            }
+        }
+    }
+}
