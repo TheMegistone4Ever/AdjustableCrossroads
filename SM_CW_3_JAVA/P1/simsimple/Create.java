d6955712d8632cdf5033b1cc4f1b9a613200b780
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-30 01:34:11 +0200

    Do refactor.

--- SM_CW_3_JAVA/P1/simsimple/Create.java
+++ SM_CW_3_JAVA/P1/simsimple/Create.java
@@ -1,5 +1,7 @@
 package SM_CW_3_JAVA.P1.simsimple;
 
+import SM_CW_2_JAVA.P1.simsimple.IElement;
+
 public class Create extends Element {
     public Create(String nameOfElement, double delay) {
         super(nameOfElement, delay);
@@ -11,15 +13,24 @@ public class Create extends Element {
         super.setTNext(.0); // імітація розпочнеться з події Create
     }
 
+    public Create(String nameOfElement, double delay, double initialTNext) {
+        super(nameOfElement, delay);
+        super.setTNext(initialTNext);
+    }
+
     @Override
     public void outAct() {
         super.outAct();
         super.setTNext(super.getTCurr() + super.getDelay());
-        ITask task = new Task();
+        ITask task = createTask();
         Path path = super.getNextPath(task);
-        Element to = path.getTo();
+        IElement to = path.getTo();
         if (to != null && !path.isBlocked(task)) {
             to.inAct(task);
         }
+    }
+
+    protected Task createTask() {
+        return new Task(super.getTCurr());
     }
 }
