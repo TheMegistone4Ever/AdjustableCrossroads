3be3311dc7e8e0a95a56e9d896979b27f55d2747
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-10-30 01:34:27 +0200

    Do refactor.

--- SM_CW_3_JAVA/P1/simsimple/Task.java
+++ SM_CW_3_JAVA/P1/simsimple/Task.java
@@ -1,7 +1,18 @@
 package SM_CW_3_JAVA.P1.simsimple;
 
 public class Task implements ITask {
+    private static int nextId = 0;
+    private final int id = nextId++;
     private double tNext = Double.MAX_VALUE;
+    private double timeIn = 0;
+
+    public Task() {
+    }
+
+    public Task(double timeIn) {
+        this.timeIn = timeIn;
+        tNext = timeIn;
+    }
 
     public double getTNext() {
         return tNext;
@@ -9,5 +20,19 @@ public class Task implements ITask {
 
     public void setTNext(double tNext) {
         this.tNext = tNext;
+    }
+
+    @Override
+    public double getTimeIn() {
+        return timeIn;
+    }
+
+    public void setTimeIn(double timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    @Override
+    public int getId() {
+        return id;
     }
 }
