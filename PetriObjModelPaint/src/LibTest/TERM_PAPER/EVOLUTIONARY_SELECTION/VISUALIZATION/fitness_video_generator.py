3d1c76dd276bbc3a000cf3bfe929a02a57b1f1f6
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-12-08 02:30:17 +0200

    Do refactor.

--- PetriObjModelPaint/src/LibTest/TERM_PAPER/EVOLUTIONARY_SELECTION/VISUALIZATION/fitness_video_generator.py
+++ PetriObjModelPaint/src/LibTest/TERM_PAPER/EVOLUTIONARY_SELECTION/VISUALIZATION/fitness_video_generator.py
@@ -21,10 +21,7 @@ def create_surface_plot(ax, X, Y, z_values, min_z, max_z):
     """
 
     z_values_clipped = np.clip(z_values, min_z, max_z)
-    ax.plot_surface(X, Y, z_values_clipped,
-                    cmap='viridis',
-                    edgecolor='none',
-                    alpha=0.6)
+    ax.plot_surface(X, Y, z_values_clipped, cmap="viridis", edgecolor="none", alpha=.6)
     ax.set_zlim(min_z, max_z)
 
 
@@ -46,36 +43,19 @@ def plot_generation_data(ax, generation_data, best_individual, phase1_values, ph
     for text in ax.texts:
         text.remove()
 
-    valid_data = generation_data[
-        (phase1_values.min() <= generation_data["1'st phase"]) &
-        (generation_data["1'st phase"] <= phase1_values.max()) &
-        (phase3_values.min() <= generation_data["3'rd phase"]) &
-        (generation_data["3'rd phase"] <= phase3_values.max())
-        ]
+    valid_data = generation_data[phase1_values.min() <= generation_data["1'st phase"] <= phase1_values.max() &
+                                 phase3_values.min() <= generation_data["3'rd phase"] <= phase3_values.max()]
 
-    ax.scatter(
-        valid_data["3'rd phase"],
-        valid_data["1'st phase"],
-        valid_data["Fitness"],
-        c='gray',
-        s=100,
-        alpha=0.8
-    )
+    ax.scatter(valid_data["3'rd phase"], valid_data["1'st phase"], valid_data["Fitness"], c="k", s=100, alpha=.8)
 
-    ax.scatter(
-        best_individual["3'rd phase"],
-        best_individual["1'st phase"],
-        best_individual["Fitness"],
-        c='red',
-        s=200,
-        alpha=0.9
-    )
+    ax.scatter(best_individual["3'rd phase"], best_individual["1'st phase"], best_individual["Fitness"], c="r", s=100,
+               alpha=.9)
 
-    ax.text2D(0.05, 0.95, f"Generation: {gen}",
-              transform=ax.transAxes, fontsize=16)
+    ax.text2D(.05, .95, f"Generation: {gen}", transform=ax.transAxes, fontsize=24)
 
 
-def create_3d_surface_animation(phase1_values, phase3_values, z_values, fitness_df):
+def create_3d_surface_animation(phase1_values, phase3_values, z_values, fitness_df,
+                                file_path="3d_surface_animation.mp4"):
     """
     Створення 3D анімації поверхні придатності.
 
@@ -84,31 +64,32 @@ def create_3d_surface_animation(phase1_values, phase3_values, z_values, fitness_
         phase3_values (np.ndarray): Значення для осі 3.
         z_values (np.ndarray): Значення функції придатності.
         fitness_df (pd.DataFrame): DataFrame з даними придатності кожної генерації.
+        file_path: Шлях до файлу для збереження анімації.
     """
 
-    fig = plt.figure(figsize=(20, 20), dpi=200)
-    ax = fig.add_subplot(111, projection='3d')
+    fig = plt.figure(figsize=(16, 16), dpi=200)
+    ax = fig.add_subplot(111, projection="3d")
 
-    ax.set_xlabel("Фаза 3", fontsize=10)
-    ax.set_ylabel("Фаза 1", fontsize=10)
-    ax.set_zlabel("Функція придатності (max(L_i))", fontsize=10)
-    ax.set_title("3D Поверхневий графік метрик", fontsize=12)
+    ax.set_xlabel("Фаза 3", fontsize=14)
+    ax.set_ylabel("Фаза 1", fontsize=14)
+    ax.set_zlabel("Функція придатності (max(L_i))", fontsize=14)
+    ax.set_title("3D Поверхневий графік метрик", fontsize=20)
 
     X, Y = np.meshgrid(phase3_values, phase1_values)
 
-    min_z, max_z = fitness_df['Fitness'].min(), fitness_df['Fitness'].max()
+    min_z, max_z = fitness_df["Fitness"].min(), fitness_df["Fitness"].max()
 
     create_surface_plot(ax, X, Y, z_values, min_z, max_z)
 
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('3d_surface_animation_opencv.mp4', fourcc, 10.0, (1600, 1600))
+    fourcc = cv2.VideoWriter_fourcc(*"avc1")
+    out = cv2.VideoWriter(file_path, fourcc, 10., (1600, 1600))
 
     best_individuals = {}
-    for gen in fitness_df['Generation'].unique():
-        gen_data = fitness_df[fitness_df['Generation'] == gen]
-        best_individuals[gen] = gen_data.loc[gen_data['Fitness'].idxmin()]
+    for gen in fitness_df["Generation"].unique():
+        gen_data = fitness_df[fitness_df["Generation"] == gen]
+        best_individuals[gen] = gen_data.loc[gen_data["Fitness"].idxmin()]
 
-    for gen in fitness_df['Generation'].unique():
+    for gen in fitness_df["Generation"].unique():
         generation_data = fitness_df[fitness_df["Generation"] == gen]
         best_individual = best_individuals[gen]
 
@@ -122,8 +103,8 @@ def create_3d_surface_animation(phase1_values, phase3_values, z_values, fitness_
 
         fig = plt.gcf()
         fig.canvas.draw()
-        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-        image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        image = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+        image = image.reshape(fig.canvas.get_width_height()[::-1] + (4,))
 
         image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         image_resized = cv2.resize(image_bgr, (1600, 1600))
@@ -133,7 +114,7 @@ def create_3d_surface_animation(phase1_values, phase3_values, z_values, fitness_
 
     out.release()
     plt.close()
-    print('3D surface animation saved as "3d_surface_animation_opencv.mp4"')
+    print(f"3D surface animation saved as \"{file_path}\"")
 
 
 def main():
